e60f494607f7de9153efc403712a00c269c34897
Freehandtree <freehandtree@gmail.com>
2025-03-13 10:42:14 +0100

    Implemented Feed Window

--- App/src/Window/Feed.java
+++ App/src/Window/Feed.java
@@ -1,8 +1,6 @@
 package Window;
 import java.awt.*;
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 
 public class Feed extends Window {
     public Feed() {
